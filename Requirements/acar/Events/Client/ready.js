03824c2a1513debb8a527b045278ccdf00c8fae1
Şehira <76520958+sehirax@users.noreply.github.com>
2022-03-24 12:23:24 +0300

    Add files via upload

---
+++ Requirements/acar/Events/Client/ready.js
@@ -0,0 +1,21 @@
+const {} = require('discord.js');
+
+ /**
+ * @param {Client} client 
+ */
+
+module.exports = async () => {
+    let sesKanalı = client.channels.cache.get(sistem.botSesKanali);
+    if (sesKanalı) sesKanalı.join().catch(err => {});
+    client.logger.log(`ACAR Moderation Plus Activated.`, "ready");
+    client.user.setActivity(sistem.botDurum.desc, {
+        type: sistem.botDurum.type,
+        url: sistem.botDurum.url,
+    });
+    
+};
+
+module.exports.config = {
+    Event: "ready"
+};
+
