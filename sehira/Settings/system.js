d8995c193eb595e896e26991c9c6cbd206ddbd15
Şehira <76520958+sehirax@users.noreply.github.com>
2022-03-24 12:32:15 +0300

    Add files via upload

---
+++ sehira/Settings/system.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+    MODTOKEN: "",
+    STATTOKEN: "",
+    MPLUSTOKEN: "",
+
+    SECTOKENS: {
+        ONE: "",
+        TWO: "",
+        THREE: "",
+        FOUR: "",
+        DISTMAIN: "",
+        DISTS: [
+        "",
+        ""
+        ]
+    },
+
+    WELCOMETOKENS: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ],
+
+    WELCOMECHANNELS: [
+        ""
+    ],
+
+    Webclient: {
+        clientID: "",
+        secret: "",
+        callbackURL: "",
+    },
+    prefix: ".",
+    prefixs: ["."],
+    MongoURL: "",
+
+    botDurum: {
+        desc: "Nexus Developed by SEHIRΛ",
+        type: "PLAYING",
+        url: "",
+    },
+
+    botSesKanali: "938133281944252518",
+
+    staff: [
+        { id: "935539319614078986", username: "sehira#0001" },
+        { id: "317518644705755138", username: "acar#0001" },
+
+    ]
+}
