bc64e4beb2e6da2eb27f3e83eba48e52fd6a3cd6
Şehira <76520958+sehirax@users.noreply.github.com>
2022-03-24 12:35:52 +0300

    Add files via upload

---
+++ Firewall_ONE/acar/Events/Client/ready.js
@@ -0,0 +1,18 @@
+const {} = require('discord.js');
+
+ /**
+ * @param {Client} client 
+ */
+
+module.exports = async () => {
+    let sesKanalı = client.channels.cache.get(sistem.botSesKanali);
+    if (sesKanalı) sesKanalı.join().catch(err => {});
+    client.user.setActivity(sistem.botDurum.desc, {
+        type: sistem.botDurum.type,
+        url: sistem.botDurum.url,
+    });
+};
+
+module.exports.config = {
+    Event: "ready"
+};
